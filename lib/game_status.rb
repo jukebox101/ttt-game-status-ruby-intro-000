20750fe9c16dd7fb579e90a9667a799d74616111
Leonora Squires <leonora.squires@gmail.com>
2020-02-21 02:26:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,45 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+  top_row_win = WIN_COMBINATIONS[0]
+  mid_row_win = WIN_COMBINATIONS[1]
+  bot_row_win = WIN_COMBINATIONS[2]
+  first_col_win = WIN_COMBINATIONS[3]
+  sec_col_win = WIN_COMBINATIONS[4]
+  third_col_win = WIN_COMBINATIONS[5]
+  left_dia_win = WIN_COMBINATIONS[6]
+  right_dia_win = WIN_COMBINATIONS[7]
+  
+def won?(board)
+  if board == [" "," "," "," "," "," "," "," "," "]
+    false
+  else
+    board.each do |combo|
+      board.each do |position|
+        t_f = board.all? |position| position == 'X'
+        if t_f == true
+          puts combo
+        else
+          false
+        end
+    end
+  end
+  end
+end
+
+def full?(board)
+  board.none? do |elements| 
+    board[elements] == " "
+    
+  end
+end
